0d4045b9a5d4f7999995c9cc699100618b882f0e
nyynyy <39200156+nyynyy@users.noreply.github.com>
2020-02-21 00:03:03 +0630

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,11 @@
+echo "# blogger" >> README.md
+
+git init
+
+git add README.md
+
+git commit -m "first commit"
+
+git remote add origin git@github.com:nyynyy/blogger.git
+
+git push -u origin master
